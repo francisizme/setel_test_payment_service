f18296e1c84c305f580e6ef540abb35b9b2eba53
Tan Huynh <tan.huynh@fractal.vn>
2021-05-10 02:46:56 +0700

    Update amqp config

--- src/listener.ts
+++ src/listener.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: [config.amqp.url],
-      queue: config.amqp.queue,
+      queue: config.amqp.payment_queue,
       queueOptions: {
         durable: false,
       },
