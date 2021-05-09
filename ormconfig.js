e5934176504a3f526e3cbf5d71a5c476bffcfa73
Tan Huynh <tan.huynh@fractal.vn>
2021-05-09 22:58:50 +0700

    Update typeorm config

--- ormconfig.js
+++ ormconfig.js
@@ -9,6 +9,7 @@ module.exports = {
   password: config.database.pass,
   database: config.database.dbName,
   entities: ['dist/src/modules/**/entities/*.entity.js'],
+  synchronize: true,
   migrationsTableName: 'migrations',
   migrations: ['dist/database/migration/*.js'],
   cli: {
